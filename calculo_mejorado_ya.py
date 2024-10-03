d5bdae11cd92dc8ed28cf56833ce39a360cb8690
Alfredo Mejia <amejia@finkok.com.mx>
2024-10-02 18:14:48 -0600

    demas

--- calculo_mejorado_ya.py
+++ calculo_mejorado_ya.py
@@ -1,16 +1,17 @@
 from collections import defaultdict
+from datetime import datetime, timedelta
 from decimal import Decimal
 from sys import orig_argv
 from lxml import etree
 from colorama import Fore, Style
+import math
 
-Token= "a0d9d68a166b6565e59872885d9bb2927abd03f00acdcadded548b96684dc93d"
-URL = "https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF43718/datos/{{fecha_ini}}/{{fecha_fin}}?token={Token}}"
+import requests
 
 
 # Cargar el archivo XML
 xml_string = open(
-    "/home/soporte-residentes/Documentos/Tareas_Alfredo/cfdi.xml", "rb").read()
+    "/home/amejia/Documentos/calculo_xd/cce.xml", "rb").read()
 xml_etree = etree.fromstring(xml_string)
 
 namespaces = {
@@ -19,6 +20,49 @@ namespaces = {
     'pago20': 'http://www.sat.gob.mx/Pagos20',
     'cce20': 'http://www.sat.gob.mx/ComercioExterior20'
 }
+
+
+def truncar(valor, decimales=2):
+    """Truncar un valor a un número de decimales especificado."""
+    factor = Decimal(10) ** decimales
+    return math.trunc(Decimal(valor) * factor) / factor
+
+
+def obtener_tipo_cambio_api():
+    token = "a0d9d68a166b6565e59872885d9bb2927abd03f00acdcadded548b96684dc93d"
+    url = "https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF43718/datos/{fecha_ini}/{fecha_fin}?token={token}"
+
+    today = datetime.now()
+    format = today.strftime('%Y-%m-%d')
+
+    # Inicializar la fecha de inicio
+    fecha_ini = format
+
+    # Intentar hasta tres días atrás si la respuesta está vacía
+    for i in range(4):  # Del día actual hasta tres días antes
+        fecha_ini = (today - timedelta(days=i)).strftime('%Y-%m-%d')
+        fecha_fin = format  # Fecha actual
+
+        # Formatear la URL
+        formatted_url = url.format(
+            fecha_ini=fecha_ini, fecha_fin=fecha_fin, token=token)
+
+        # Realizar la solicitud GET
+        response = requests.get(formatted_url)
+
+        # Verificar el estado de la respuesta
+        if response.status_code == 200:
+            data = response.json()
+            if data['bmx']['series'][0]['datos']:  # Comprobar si hay datos
+                tipo_cambio_api = float(
+                    data['bmx']['series'][0]['datos'][0]['dato'])
+                print(f"Tipo de cambio API Banxico: {tipo_cambio_api}")
+                return tipo_cambio_api
+            else:
+                print(f"No hay datos para la fecha: {fecha_ini}")
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+    return None
 
 
 def realizar_calculos_generales():
@@ -145,9 +189,102 @@ def calcular_retenciones():
 
     print(Fore.GREEN + "----------------------------------------------\n" + Style.RESET_ALL)
 
+# Función para realizar cálculos relacionados con Comercio Exterior
+
 
 def realizar_calculos_comercio_exterior():
-    print("calculos ")
+    print(Fore.GREEN + "-------------- Cálculos Comercio Exterior --------------" + Style.RESET_ALL)
+
+    # Obtener el tipo de cambio desde el XML (ya integrado)
+    try:
+        tipo_cambio_xml = xml_etree.xpath(
+            ".//cce20:ComercioExterior/@TipoCambioUSD", namespaces=namespaces)
+        if tipo_cambio_xml:
+            tipo_cambio_xml = float(tipo_cambio_xml[0])
+            print(Fore.BLUE + f"Tipo de cambio XML (Comercio Exterior): " +
+                  Fore.YELLOW + f"{tipo_cambio_xml}" + Style.RESET_ALL)
+        else:
+            print(
+                Fore.RED + "No se encontró el atributo TipoCambioUSD en el XML." + Style.RESET_ALL)
+            return
+    except Exception as e:
+        print(Fore.RED + f"Error al leer el XML: {e}" + Style.RESET_ALL)
+        return
+
+    # Obtener el tipo de cambio desde la API
+    tipo_cambio_api = obtener_tipo_cambio_api()
+
+    if tipo_cambio_api is not None:
+        if tipo_cambio_xml == tipo_cambio_api:
+            print(Fore.GREEN + "Los tipos de cambio coinciden." + Style.RESET_ALL)
+        else:
+            print(
+                Fore.RED + f"Los tipos de cambio NO coinciden. XML: " + Fore.YELLOW + f"{tipo_cambio_xml}" + Fore.RED + f", API: " + Fore.YELLOW + f"{tipo_cambio_api}" + Style.RESET_ALL)
+    else:
+        print(
+            Fore.RED + "No se pudo obtener el tipo de cambio de la API." + Style.RESET_ALL)
+
+    # Validaciones adicionales para las mercancías
+    mercancias = xml_etree.xpath(
+        ".//cce20:ComercioExterior/cce20:Mercancias/cce20:Mercancia", namespaces=namespaces)
+
+    for mercancia in mercancias:
+        no_identificacion = mercancia.get("NoIdentificacion")
+        valor_dolares = float(mercancia.get("ValorDolares", "0"))
+        cantidad_aduana = mercancia.get("CantidadAduana")
+        valor_unitario_aduana = mercancia.get("ValorUnitarioAduana")
+
+        # Obtener los decimales del nodo Concepto que coinciden con NoIdentificacion
+        concepto = xml_etree.xpath(
+            f".//cfdi:Concepto[@NoIdentificacion='{no_identificacion}']", namespaces=namespaces)
+
+        if concepto:
+            concepto = concepto[0]
+            cantidad = concepto.get("Cantidad", "0")
+            valor_unitario = concepto.get("ValorUnitario", "0")
+
+            # Calcular el número de decimales de la cantidad y el valor unitario
+            num_decimales_cantidad = len(
+                cantidad.split('.')[-1]) if '.' in cantidad else 0
+            num_decimales_valor_unitario = len(
+                valor_unitario.split('.')[-1]) if '.' in valor_unitario else 0
+        else:
+            num_decimales_cantidad, num_decimales_valor_unitario = None, None
+
+        if cantidad_aduana and valor_unitario_aduana and num_decimales_cantidad is not None and num_decimales_valor_unitario is not None:
+            cantidad_aduana = float(cantidad_aduana)
+            valor_unitario_aduana = float(valor_unitario_aduana)
+
+            # Evitar validaciones si ValorDolares es "0" o "1"
+            if valor_dolares in [0, 1]:
+                continue
+
+            # Cálculo del límite inferior
+            cantidad_inferior = cantidad_aduana - \
+                (10 ** -num_decimales_cantidad) / 2
+            valor_inferior = valor_unitario_aduana - \
+                (10 ** -num_decimales_valor_unitario) / 2
+            limite_inferior = cantidad_inferior * valor_inferior
+            # Truncar a dos decimales
+            limite_inferior_truncado = truncar(limite_inferior, 2)
+
+            # Cálculo del límite superior
+            cantidad_superior = cantidad_aduana + \
+                (10 ** -num_decimales_cantidad) / 2 - 10 ** -12
+            valor_superior = valor_unitario_aduana + \
+                (10 ** -num_decimales_valor_unitario) / 2 - 10 ** -12
+            limite_superior = cantidad_superior * valor_superior
+            # Redondear al número entero más cercano
+            limite_superior_redondeado = round(limite_superior)
+
+            # Validar que el valor de ValorDolares esté dentro de los límites
+            if limite_inferior_truncado > valor_dolares or valor_dolares > limite_superior_redondeado:
+                print(
+                    Fore.RED + f"El valor de ValorDolares (" + Fore.YELLOW + f"{valor_dolares}" + Fore.RED + f") está fuera de los límites para la mercancía con NoIdentificacion: {no_identificacion}." + Style.RESET_ALL)
+                print(Fore.CYAN + f"Limite inferior: " + Fore.YELLOW +
+                      f"{limite_inferior_truncado}" + Fore.CYAN + f"--"+Fore.CYAN + f"Limite superior: "+Fore.YELLOW + f"{limite_superior_redondeado}")
+
+    print(Fore.GREEN + "--------------------------------------------------------" + Style.RESET_ALL)
 
 
 # Verificar el tipo de comprobante y proceder
@@ -166,6 +303,7 @@ if tipo_comprobante:
 
     elif tipo in ["I", "E"]:
         realizar_calculos_generales()
+        realizar_calculos_comercio_exterior()
 
     elif tipo == 'P':
         realizar_calculos_pago()
