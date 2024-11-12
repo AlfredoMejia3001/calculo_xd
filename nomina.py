38a15de32fb1850ac10beeb1c9d0c537731e3e55
Soporte Residentes <soporte-residentes@residente2.oficina.yezdo.com>
2024-11-12 12:28:00 -0600

    calculos listos, solo falta que se vean bonitos los prints

--- nomina.py
+++ nomina.py
@@ -52,13 +52,13 @@ def redondeo(valor):
         return Decimal(valor.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP))
     
 
-def valor_unitario():
+def valor_unitario(namespaces):
     valor_unitario = xml_etree.xpath("//cfdi:Conceptos/cfdi:Concepto/@ValorUnitario", namespaces=namespaces)
     total_otros_pagos = xml_etree.xpath("//nomina12:Nomina/@TotalOtrosPagos", namespaces=namespaces)
     total_persepciones = xml_etree.xpath("//nomina12:Nomina/@TotalPercepciones", namespaces=namespaces)
     
-    tot_pag = float(total_otros_pagos[0])
-    tot_per = float(total_persepciones[0])
+    tot_pag = float(total_otros_pagos[0]) if total_otros_pagos else 0
+    tot_per = float(total_persepciones[0]) if total_persepciones else 0
     suma= tot_pag + tot_per
     if suma != float(valor_unitario[0]):
         print("no son iguales val uni")
@@ -68,23 +68,23 @@ def valor_unitario():
     print("suma:",suma)
 
 
-def importe():
+def importe(namespaces):
     imp = xml_etree.xpath("//cfdi:Conceptos/cfdi:Concepto/@Importe", namespaces=namespaces)
     total_otros_pagos = xml_etree.xpath("//nomina12:Nomina/@TotalOtrosPagos", namespaces=namespaces)
     total_persepciones = xml_etree.xpath("//nomina12:Nomina/@TotalPercepciones", namespaces=namespaces)
     
-    tot_pag = float(total_otros_pagos[0])
-    tot_per = float(total_persepciones[0])
+    tot_pag = float(total_otros_pagos[0]) if total_otros_pagos else 0
+    tot_per = float(total_persepciones[0]) if total_persepciones else 0
     suma= tot_pag + tot_per
     if suma != float(imp[0]):
-        print("no son iguales")
+        print("no son iguales importe")
     else:
-        print("si son iguales")
+        print("si son iguales importe")
         
     print("suma:",suma)
 
 
-def descuento ():
+def descuento (namespaces):
     desc = xml_etree.xpath("//cfdi:Conceptos/cfdi:Concepto/@Descuento", namespaces=namespaces)
     total_deduciones = xml_etree.xpath("//nomina12:Nomina/@TotalDeducciones", namespaces=namespaces)
     des = str(desc[0])
@@ -92,10 +92,10 @@ def descuento ():
     if des != total_deducione:
         print("El descuento no coincide")
     else:
-        print("Todo esta correcto")
+        print("Todo esta correcto descuento")
 
         
-def total_percepciones():
+def total_percepciones(namespaces):
     total_perp = xml_etree.xpath("//nomina12:Nomina/@TotalPercepciones", namespaces=namespaces)
 
     total_sueldos = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/@TotalSueldos", namespaces=namespaces)
@@ -116,7 +116,7 @@ def total_percepciones():
 
 
 
-def total_desucciones():
+def total_desucciones(namespaces):
     total_deduc = xml_etree.xpath("//nomina12:Nomina/@TotalDeducciones", namespaces=namespaces)
 
     total_otras_deducciones = xml_etree.xpath("//nomina12:Nomina/nomina12:Deducciones/@TotalOtrasDeducciones", namespaces=namespaces)
@@ -133,7 +133,7 @@ def total_desucciones():
         print("Son iguales las totales deducciones ")
 
 
-def total_otros_pagos():
+def total_otros_pagos(namespaces):
     imp = (xml_etree.xpath("//nomina12:Nomina/nomina12:OtrosPagos/nomina12:OtroPago/@Importe", namespaces=namespaces))
     total_otros_pagos= xml_etree.xpath("//nomina12:Nomina/@TotalOtrosPagos", namespaces=namespaces)
     
@@ -144,25 +144,50 @@ def total_otros_pagos():
         print('son iguales')
 
 
-def monto_recurso_propio():
+def monto_recurso_propio(namespaces):
     
     total_otros_pagos = xml_etree.xpath("//nomina12:Nomina/@TotalOtrosPagos", namespaces=namespaces)
     total_persepciones = xml_etree.xpath("//nomina12:Nomina/@TotalPercepciones", namespaces=namespaces)
     monto_rec = xml_etree.xpath("//nomina12:Nomina/nomina12:Emisor/nomina12:EntidadSNCF/@MontoRecursoPropio", namespaces=namespaces)
-    total_otros_pagos = float(total_otros_pagos[0])
-    total_persepciones = float(total_persepciones[0])
+    total_otros_pagos = float(total_otros_pagos[0]) if total_otros_pagos else 0
+    total_persepciones = float(total_persepciones[0]) if total_persepciones else 0
     monto = total_otros_pagos + total_persepciones
 
-    print("percepciones")
-    print(monto)
-    print(monto_rec[0])
-    if monto != float(monto_rec[0]):
-        print("Son diferentes ")
+   
+    if monto != float(monto_rec[0]) if monto_rec else 0:
+        print(f"es incorrrecto el monto recurso propio, este es el resultado correcto del calculo: {monto}")
     else:
-        print("son iguales")
+        print("es correcto el monto de recursos propios")
 
 
-def total_sueldos():
+def percep_val(namespaces):
+    total_sueldos = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/@TotalSueldos", namespaces=namespaces)
+    total_separacion = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/@TotalSeparacionIndemnizacion", namespaces=namespaces)
+    total_jubilacion = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/@TotalJubilacionPensionRetiro", namespaces=namespaces)
+    
+    total_sueldos = float(total_sueldos[0]) if total_sueldos else 0
+    total_separacion = float(total_separacion[0]) if total_separacion else 0
+    total_jubilacion = float(total_jubilacion[0]) if total_jubilacion else 0
+
+    percep= total_sueldos + total_separacion + total_jubilacion
+
+    total_gravado = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/@TotalGravado", namespaces=namespaces)
+    total_excento = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/@TotalExento", namespaces=namespaces)
+
+    total_gravado = float(total_gravado[0]) if total_gravado else 0
+    total_excento = float(total_excento[0]) if total_excento else 0
+
+    grav = total_gravado + total_excento
+
+    if percep != grav:
+        print('No coinciden las sumas de TotalSueldos + TotalSeparacionIndemnizacion + TotalJubilacionPensionRetiro')
+        print('Y la suma de TotalGravado + TotalExento')
+    else:
+        print('Si coinciden las sumas de TotalSueldos + TotalSeparacionIndemnizacion + TotalJubilacionPensionRetiro ')
+        print('Y la suma de TotalGravado + TotalExento')
+
+
+def total_sueldos(namespaces):
     total_sueldos = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/@TotalSueldos", namespaces=namespaces)
     ImporteGravado = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/nomina12:Percepcion/@ImporteGravado", namespaces=namespaces)
     ImporteExento = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/nomina12:Percepcion/@ImporteExento", namespaces=namespaces)
@@ -175,12 +200,12 @@ def total_sueldos():
     tot_suel = ImporteExent + ImporteGravad
 
     if tot_suel != float(total_sueldos[0]):
-        print("no son iguales")
+        print(f"El TotalSueldos no es correcto, calculo hecho:{tot_suel}")
     else:
-        print("son iguales")
+        print("son iguales total sueldos")
 
 
-def total_separacion():
+def total_separacion(namespaces):
     total_separacion = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/@TotalSeparacionIndemnizacion", namespaces=namespaces)
     ImporteGravado = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/nomina12:Percepcion/@ImporteGravado", namespaces=namespaces)
     ImporteExento = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/nomina12:Percepcion/@ImporteExento", namespaces=namespaces)
@@ -193,12 +218,12 @@ def total_separacion():
     tot_suel = ImporteExent + ImporteGravad
 
     if tot_suel != float(total_separacion[0]):
-        print("no son iguales")
+        print(f"no son iguales total separacion XML:{total_separacion[0]} != calculo: {tot_suel}")
     else:
-        print("son iguales")
+        print("son iguales separacion")
 
 
-def total_jubilacion():
+def total_jubilacion(namespaces):
     total_jubilacion = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/@TotalJubilacionPensionRetiro", namespaces=namespaces)
     ImporteGravado = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/nomina12:Percepcion/@ImporteGravado", namespaces=namespaces)
     ImporteExento = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/nomina12:Percepcion/@ImporteExento", namespaces=namespaces)
@@ -211,50 +236,42 @@ def total_jubilacion():
     tot_suel = ImporteExent + ImporteGravad
 
     if tot_suel != float(total_jubilacion[0]):
-        print("no son iguales")
+        print(f"no son iguales total jubilacion XML:{total_jubilacion[0]} calculo: {tot_suel}")
     else:
-        print("son iguales")
+        print("son iguales jubilacion")
 
 
-def total_gravado():
+def total_gravado(namespaces):
     total_gravado = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/@TotalGravado", namespaces=namespaces)
 
     ImporteGravado = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/nomina12:Percepcion/@ImporteGravado", namespaces=namespaces)
-    ImporteExento = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/nomina12:Percepcion/@ImporteExento", namespaces=namespaces)
     
     ImporteGravad = sum(float (impor) for impor in ImporteGravado )
-    ImporteExent = sum(float (impor) for impor in ImporteExento )
 
-    ImporteExento = float(ImporteExento[0])
-    ImporteGravado = float(ImporteGravado[0])
-    tot_suel = ImporteExent + ImporteGravad
+ 
 
-    if tot_suel != float(total_gravado[0]):
-        print("no son iguales")
+    if ImporteGravad != float(total_gravado[0]):
+        print(f"no son iguales total gravado XML:{total_gravado[0]} calculo: {ImporteGravad}")
     else:
-        print("son iguales")
+        print("son iguales gravado")
     
     
-def total_excemto():
+def total_excemto(namespaces):
     total_excento = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/@TotalExento", namespaces=namespaces)
 
-    ImporteGravado = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/nomina12:Percepcion/@ImporteGravado", namespaces=namespaces)
     ImporteExento = xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones/nomina12:Percepcion/@ImporteExento", namespaces=namespaces)
     
-    ImporteGravad = sum(float (impor) for impor in ImporteGravado )
     ImporteExent = sum(float (impor) for impor in ImporteExento )
 
-    ImporteExento = float(ImporteExento[0])
-    ImporteGravado = float(ImporteGravado[0])
-    tot_suel = ImporteExent + ImporteGravad
-
-    if tot_suel != float(total_excento[0]):
-        print("no son iguales")
-    else:
-        print("son iguales")
     
 
-def total_imp_reten_deduc():
+    if ImporteExent != float(total_excento[0]):
+        print(f"no son iguales total excento XML:{total_excento[0]} Calculo: {ImporteExent}")
+    else:
+        print("son iguales Excento")
+    
+
+def total_imp_reten_deduc(namespaces):
     total_impuestos_retenidos = xml_etree.xpath("//nomina12:Deducciones/@TotalImpuestosRetenidos", namespaces=namespaces)    
     deducciones_002 = xml_etree.xpath("//nomina12:Deducciones/nomina12:Deduccion[@TipoDeduccion='002']/@Importe", namespaces=namespaces)
 
@@ -273,4 +290,36 @@ def total_imp_reten_deduc():
             print("No hay deducciones TipoDeduccion=002 y no se debe incluir el atributo TotalImpuestosRetenidos.")
 
 
-total_imp_reten_deduc()
+
+
+
+
+def condicion_nomina(namespaces):
+    valor_unitario(namespaces)
+    importe(namespaces)
+    descuento(namespaces)
+    if xml_etree.xpath("//nomina12:Nomina/nomina12:Percepciones", namespaces=namespaces):
+        total_percepciones(namespaces)
+    if xml_etree.xpath("//nomina12:Nomina/nomina12:Deducciones", namespaces=namespaces):
+        total_desucciones(namespaces)
+    if xml_etree.xpath("//nomina12:Nomina/nomina12:OtrosPagos", namespaces=namespaces):
+        total_otros_pagos(namespaces)
+    monto_recurso_propio(namespaces)
+    percep_val(namespaces)
+    if xml_etree.xpath('//nomina12:Nomina/nomina12:Percepciones/nomina12:Percepcion[@TipoPercepcion="022" or @TipoPercepcion="023" or @TipoPercepcion="025" or @TipoPercepcion="039" or @TipoPercepcion="044"]', namespaces=namespaces):
+        print
+    else:
+        total_sueldos(namespaces)
+    
+    if xml_etree.xpath('//nomina12:Nomina/nomina12:Percepciones/nomina12:Percepcion[@TipoPercepcion="022" or @TipoPercepcion="023" or @TipoPercepcion="025"]', namespaces=namespaces):
+        total_separacion(namespaces)
+
+    if xml_etree.xpath('//nomina12:Nomina/nomina12:Percepciones/nomina12:Percepcion[@TipoPercepcion="039" or @TipoPercepcion="044"]', namespaces=namespaces):
+        total_sueldos(namespaces)
+
+    total_gravado(namespaces)
+    total_excemto(namespaces)
+    if xml_etree.xpath("//nomina12:Deducciones/nomina12:Deduccion[@TipoDeduccion='002']", namespaces=namespaces):
+        total_imp_reten_deduc(namespaces)
+
+condicion_nomina(namespaces)
