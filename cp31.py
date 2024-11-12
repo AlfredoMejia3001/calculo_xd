ec024615d7be7e907c4ca2b9bd08114eb2543758
Soporte Residentes <soporte-residentes@residente2.oficina.yezdo.com>
2024-11-12 12:29:15 -0600

    calculos terminados creo

--- cp31.py
+++ cp31.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 from lxml import etree
 from colorama import Fore, Style
 
-xml_string = open('prueba.xml', 'rb').read()
+xml_string = open('prueba2.xml', 'rb').read()
 xml_etree = etree.fromstring(xml_string)
 
 namespaces = {
@@ -15,6 +15,8 @@ namespaces = {
     'cartaporte31': 'http://www.sat.gob.mx/CartaPorte31',
     's0': 'apps.services.soap.core.views'
 }
+
+
 fecha = datetime.now()
 fecha = fecha.strftime('%Y-%m-%dT%H:%M:%S')
 rfc="EKU9003173C9"
@@ -24,17 +26,20 @@ xml_etree.set("Fecha", fecha)
 xml_etree.set("NoCertificado", '30001000000500003416')
 nodo = xml_etree.xpath(".//cfdi:Emisor", namespaces=namespaces)
 
+
 if nodo:  
     emisor = nodo[0]  
     emisor.set('Rfc', str(rfc))
     emisor.set('Nombre', str(name))
     emisor.set('RegimenFiscal', str(regi))
 
-def realizar_calculos_cartaporte31():
+
+def realizar_calculos_cartaporte31(namespaces):
     if xml_etree.xpath('.//cartaporte31:Mercancias/cartaporte31:Autotransporte', namespaces=namespaces):
         pesoKG=xml_etree.xpath('.//cartaporte31:Mercancias/cartaporte31:Mercancia/@PesoEnKg', namespaces=namespaces)
         pesoBrutoTotal= xml_etree.xpath('.//cartaporte31:Mercancias/@PesoBrutoTotal', namespaces=namespaces)
         suma_peso = sum(float(peso) for peso in pesoKG)
+        
         if suma_peso != float(pesoBrutoTotal[0]):
             suma_pesoSTR=int(suma_peso)
             if suma_pesoSTR != int(pesoBrutoTotal[0]):
@@ -46,12 +51,14 @@ def realizar_calculos_cartaporte31():
         else:
             print('Los pesos coinciden')
             print(f"{suma_peso} = {pesoBrutoTotal[0]}")
+
     elif xml_etree.xpath('.//cartaporte31:Mercancias/cartaporte31:TransporteMaritimo', namespaces=namespaces):
         pesoBrutoTotal= (xml_etree.xpath('.//cartaporte31:Mercancias/@PesoBrutoTotal', namespaces=namespaces))
         pesoBruto=xml_etree.xpath('.//cartaporte31:Mercancias/cartaporte31:Mercancia/cartaporte31:DetalleMercancia/@PesoBruto', namespaces=namespaces)
         pesoNeto=xml_etree.xpath('.//cartaporte31:Mercancias/cartaporte31:Mercancia/cartaporte31:DetalleMercancia/@PesoNeto', namespaces=namespaces)
         pesoNetoTotal = (xml_etree.xpath('.//cartaporte31:Mercancias/@PesoNetoTotal', namespaces=namespaces))
         suma_peso = sum(float(peso) for peso in pesoBruto)
+        
         if suma_peso != float(pesoBrutoTotal[0]):
             suma_pesoSTR=int(suma_peso)
             print(suma_pesoSTR)
@@ -76,7 +83,9 @@ def realizar_calculos_cartaporte31():
                 print(f"{suma_pesoNT} = {pesoNetoTotal[0]}")
         else:
             print('Los pesos coinciden neto 1')
-            print(f"{suma_pesoN} = {float(pesoNetoTotal[0])}")        
+            print(f"{suma_pesoN} = {float(pesoNetoTotal[0])}")      
+
+def NumTotalMercancias(namespaces):           
     mercancias = xml_etree.xpath(".//cartaporte31:Mercancias/cartaporte31:Mercancia", namespaces=namespaces)
     cantidad=len(mercancias)
     NumTotalMerca = xml_etree.xpath('.//cartaporte31:Mercancias/@NumTotalMercancias', namespaces=namespaces)
@@ -86,9 +95,13 @@ def realizar_calculos_cartaporte31():
         print(f'La cantidad de mercancias no coinciden {cantidad} != {NumTotalMerca[0]}')
     else:
         print(f'Las cantidades son iguales {cantidad} = {NumTotalMerca[0]}')
-#---------------------------------------TransporteFerroviario------------------------------------------------------#
+        
+
+#---------------------------------------TransporteFerroviario-------------------------------------------#
+def ferroviario(namespaces):   
     toneladas_netas_carro= xml_etree.xpath(".//cartaporte31:Mercancias/cartaporte31:TransporteFerroviario/cartaporte31:Carro/@ToneladasNetasCarro", namespaces=namespaces)
-    print(toneladas_netas_carro[0], "tn")
+    toneladas_netas_carro = int(toneladas_netas_carro[0])
+    print(toneladas_netas_carro, "tn")
     peso_neto_merca= xml_etree.xpath(".//cartaporte31:Mercancias/cartaporte31:TransporteFerroviario/cartaporte31:Carro/cartaporte31:Contenedor/@PesoNetoMercancia", namespaces=namespaces)
     print(peso_neto_merca[0], "kg")
     toneladas = int(peso_neto_merca[0]) / 1000
@@ -97,12 +110,13 @@ def realizar_calculos_cartaporte31():
         print(f"El PesoNetoMercancia {peso_neto_merca[0]} es diferente al de ToneladasNetasCarro {toneladas_netas_carro[0]}")
     else:
         print('Son iguales')
-#--------------------------------------------TotalDistRec------------------------------------------------#
+
+
+#--------------------------------------------TotalDistRec-----------------------------------------------#
+def total_dist(namespaces):
     total_dist_rec = xml_etree.xpath('.//cartaporte31:CartaPorte/@TotalDistRec', namespaces=namespaces)
     print(total_dist_rec[0],"km")
     distancia_recorrida = xml_etree.xpath('.//cartaporte31:Ubicaciones/cartaporte31:Ubicacion/@DistanciaRecorrida', namespaces=namespaces)
     total_dist = sum(float(dist) for dist in distancia_recorrida)
     print(total_dist, "km")
 
-
-realizar_calculos_cartaporte31()
